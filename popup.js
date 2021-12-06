a35cb75544db8896aa923341d5f32ffb1653c0ee
ryanxue <ryan.xue2@gmail.com>
2021-12-05 22:27:51 -0500

    changes

--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 // If the user clicks the clear button, remove all the sources from the reference list
 let clearButton = document.querySelector('#clear-button');
 clearButton.addEventListener('click', function() {
-    chrome.storage.sync.clear();
-    alert('Cleared');
+    if (confirm('Clear Reference List?')) {
+        chrome.storage.sync.clear();
+    }
+})
+
+let copyButton = document.querySelector('#copy-list');
+copyButton.addEventListener('click', function() {
+    navigator.clipboard.writeText('placeholder');
 })
