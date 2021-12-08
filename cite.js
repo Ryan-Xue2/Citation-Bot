f5e7a4f6d79291485386014b94451cdd2a0738e7
ryanxue <ryan.xue2@gmail.com>
2021-12-07 22:00:39 -0500

    addedstuff

--- cite.js
+++ cite.js
@@ -1,21 +1,41 @@
+// Autofill website title as well as todays date
+let websiteTitle = document.querySelector('#website-title');
+websiteTitle.value = 'placeholder';
+
+let [dayAccessed, monthAccessed, yearAccessed] = document.getElementsByName('date-accessed');
+let today = new Date();
+dayAccessed.value = today.getDate();
+monthAccessed.value = today.getMonth() + 1;
+yearAccessed.value = today.getFullYear();
+
 function citeCurrentTab() {
-    // Cite current tab and add to the reference list
+    // Cite current tab and store source info in storage
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         let url = tabs[0].url;
-        let authors = null;
-        let title = null;
-        let pubDate = null;
-        let publisher = null;
-        let dateAccesed = null;
 
-        let sourceInfo = {
+        const authors = [];
+        let fnames = document.getElementsByName('fname');
+        let lnames = document.getElementsByName('lname');
+        for (let i = 0; i < fnames.length; i++) {
+            authors.push({fname: fnames[i], lname: lnames[i]});
+        }
+
+        let title = document.querySelector('#article-title');
+        let publisher = document.querySelector('#publisher');
+        let [pubDay, pubMonth, pubYear] = document.getElementsByName('pub-date');
+        
+        const sourceInfo = {
             'url': url,
-            'authors': [{fname: null, mname: null, lname: null}],
-            'title': null,
-            'websiteTitle': null,
-            'pubDate': null,
-            'publisher': null,
-            'dateAccessed': null
+            'authors': authors,
+            'title': title.value,
+            'websiteTitle': websiteTitle.value,
+            'publisher': publisher.value,
+            'pubDay': pubDay.value,
+            'pubMonth': pubMonth.value,
+            'pubYear': pubYear.value,
+            'dayAccessed': dayAccessed.value,
+            'monthAccessed': monthAccessed.value,
+            'yearAccessed': yearAccessed.value
         };
 
         chrome.storage.sync.set({[url]: sourceInfo});
@@ -23,13 +43,27 @@ function citeCurrentTab() {
     })
 }
 
-function addAuthor() {
-    let node = document.querySelector('#authors');
-    node.innerHTML += ''
-}
-
+// Cite current tab once button clicked
 let citeButton = document.querySelector('#cite-button');
 citeButton.addEventListener('click', citeCurrentTab);
 
+// Add author once button clicked
+let authorBlock = document.querySelector('#authors');
 let addAuthorButton = document.querySelector('#add-author');
-addAuthorButton.addEventListener('click', addAuthor);
+addAuthorButton.addEventListener('click', function() {
+    authorBlock.innerHTML += '<input type="text" name="fname" placeholder="First name"><br>\
+                              <input type="text" name="lname" placeholder="Last name"><br>';
+});
+
+// Remove author when button clicked
+let removeAuthorButton = document.querySelector('#remove-author');
+removeAuthorButton.addEventListener('click', function() {
+    let nodes1 = document.getElementsByName('fname');
+    let nodes2 = document.getElementsByName('lname');
+    let nodes3 = document.getElementsByName('newline');
+    let idx = nodes1.length - 1;
+    nodes1[idx].remove();
+    nodes2[idx].remove();
+    nodes3[idx].remove();
+    nodes3[idx-1].remove();
+})
--- popup.js
+++ popup.js
@@ -1,22 +1,74 @@
-let urlList = document.querySelector('#url-list');
+let referenceList = document.querySelector('#reference-list');
+let citationFormat = '';
 
-// If the user clicks the clear button, remove all the sources from the reference list
+function abbreviateMonth(month) {
+    switch(month) {
+        case 1: return 'Jan.';
+        case 2: return 'Feb.';
+        case 3: return 'Mar.';
+        case 4: return 'Apr.';
+        case 5: return 'May';
+        case 6: return 'June';
+        case 7: return 'July';
+        case 8: return 'Aug.';
+        case 9: return 'Sept.';
+        case 10: return 'Oct.';
+        case 11: return 'Nov.';
+        case 12: return 'Dec.';
+        default: break;
+    }
+}
+function mla(item) {
+    let citation = '';
+    // Authors
+    citation += 'last name, first name.';
+    
+    // Article Title
+    citation += `"${item['title']}."`;
+
+    // Website title
+    citation += `<i>${item['websiteTitle']}</i>, `;
+
+    // Publishing information
+    citation += `${item['publisher']}, `
+    citation += `${item['pubDay']} `
+    citation += abbreviateMonth(item['month']);
+    citation += ' ';
+    citation += item['pubYear'];
+
+    // URL (remove the https:// part later)
+    citation += item['url'] + '.';
+    
+    // Date accessed
+    citation += ' ' + item['dayAccessed'];
+    citation += ' ' + abbreviateMonth(item['monthAccessed']);
+    citation += ' ' + item['yearAccessed'];
+    return citation;
+}
+
+function apa(item) {
+    return;
+}
+
+// If the user clicks the clear button, remove all selected sources from reference list
 let clearButton = document.querySelector('#clear-button');
 clearButton.addEventListener('click', function() {
     if (confirm('Clear Reference List?')) {
         chrome.storage.sync.clear();
-        urlList.innerHTML = '';
+        referenceList.innerHTML = '';
     }
 })
 
+// Copy selected sources to clipboard
 let copyButton = document.querySelector('#copy-button');
 copyButton.addEventListener('click', function() {
-    navigator.clipboard.writeText('placeholder');
+    navigator.clipboard.writeText('');
 })
 
+// Write sources to reference list in selected format
 chrome.storage.sync.get(null, function(items) {
     let urls = Object.keys(items);
     for (let i = 0; i < urls.length; i++) {
-        urlList.innerHTML += `<li>${urls[i]}</li>`;
+        referenceList.innerHTML += `<input type="checkbox" name="${urls[i]}"><label for="${urls[i]}">${mla(items[urls[i]])}</label>`;
     }
 })
